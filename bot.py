3ab1bfd90a19fc27a1a1d72031a0efd098d88b62
Yarost228 <yarik.alferov1@gmail.com>
2025-03-01 14:12:02 +0300

    Капитальная обнова
    Добавил статистику
    Добавил лидерборд
    Добавил хранение данных через JSON

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 from telebot import *
-from telebot import types
-
+import json
+import os
 
 from random import *
+
+counts = [0, 0, 0]
 
 bot = telebot.TeleBot('7932251208:AAHaoaBKRcVQQKxXAA2on9DfYeiKFJI-KC0')
 
@@ -35,13 +37,51 @@ def roll_rps(vvod, message):
     else:
         return "unknown"
 
+def writeStats(message, statlist):
+    with open(f'users/{message.from_user.username}.json', 'w+') as file:
+        json.dump(statlist, file)
+
+
+def getStats(message):
+    with open(f'users/{message.from_user.username}.json', 'r') as file:
+        return json.load(file)
+
 
 @bot.message_handler(commands=['start'])
 def startBot(message):
     bot.send_message(message.chat.id, "введите rock, paper или scissors", parse_mode='html')
+    writeStats(message, [0, 0, 0])
+
+@bot.message_handler(commands=['leaderboard'])
+def showLeaderboard(message):
+    directory = f"{os.curdir}/users"
+    leaderboard = {}
+    text = ""
+    files = os.listdir(directory)
+    for user in files:
+        with open(f'users/{user}', 'r') as file:
+            leaderboard[user] = json.load(file)[0]
+    sortedLeaderboard = sorted(leaderboard.items(), key=lambda leader: leader[1], reverse=True)
+    for user in sortedLeaderboard:
+        text += f"{user[0].replace('.json', '')}: {user[1]}\n"
+    bot.send_message(message.chat.id, text, parse_mode='html')
+
+@bot.message_handler(commands=['stats'])
+def showStats(message):
+    bot.send_message(message.chat.id, f"w {getStats(message)[0]}, l {getStats(message)[1]}, t {getStats(message)[2]}", parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
 def phrase(message):
-    bot.send_message(message.chat.id, roll_rps(message.text, message), parse_mode='html')
+    roll = roll_rps(message.text.lower(), message)
+    w, l, t = getStats(message)[0], getStats(message)[1], getStats(message)[2]
+    if roll == "you win":
+        writeStats(message, [int(w) + 1, l, t])
+    if roll == "you lose":
+        writeStats(message, [w, int(l) + 1, t])
+    if roll == "tie":
+        writeStats(message, [w, l, int(t) + 1])
+    bot.send_message(message.chat.id, roll, parse_mode='html')
+    bot.send_message(message.chat.id, "введите rock, paper или scissors", parse_mode='html')
+
 
 bot.polling(none_stop=True)
